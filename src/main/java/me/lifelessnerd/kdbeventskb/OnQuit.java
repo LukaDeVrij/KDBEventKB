3da461f9062753aa39116831d3f2ad9b6dce2a31
LifelessNerd <35869162+LifelessNerd@users.noreply.github.com>
2020-07-20 18:02:18 +0200

    world check added, and if (playerScoreInt = 1 | 2) -> do stuff

--- src/main/java/me/lifelessnerd/kdbeventskb/OnQuit.java
+++ src/main/java/me/lifelessnerd/kdbeventskb/OnQuit.java
@@ -15,21 +15,23 @@ public class OnQuit implements Listener {
     @EventHandler
     public void onPlayerQuit(final PlayerQuitEvent event) {
         Player player = event.getPlayer();
-        Scoreboard scoreboard = player.getScoreboard();
-        Objective objective = scoreboard.getObjective("KBteam");
 
-        assert objective != null : "Speler had geen score op " + objective.getName();
-        //
-        Score playerScore = objective.getScore(player);
-        int playerScoreInt = playerScore.getScore();
-        Bukkit.broadcastMessage(String.valueOf(playerScore));
-        //
+        if (player.getWorld().getName().equalsIgnoreCase("KBevent")) {
 
-//        if (player.getWorld().getName().equalsIgnoreCase("kbevent")) {
-//            if (playerScore.equals("1")) {
-//
-//            }
-//
-//        }
+            Scoreboard scoreboard = player.getScoreboard();
+            Objective objective = scoreboard.getObjective("KBteam");
+            // idk what this does but the IDE said it so sure and it works so a je to i guess
+            assert objective != null : "Speler had geen score op " + objective.getName();
+            //
+            Score playerScore = objective.getScore(player);
+            int playerScoreInt = playerScore.getScore();
+
+            if (playerScoreInt == 1 | playerScoreInt == 2) {
+                Bukkit.broadcastMessage(player + " had a score of 1 or 2: " + playerScoreInt);
+                // do stuff when player in team 1 or 2 quits
+            } else {
+                Bukkit.broadcastMessage(player + " was not team 1 or 2");
+            }
+        }
     }
 }
